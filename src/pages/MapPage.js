9072b832adfeab9a0abc756c2407b5b305a11d1a
mbartholomai <mdbartholomai@gmail.com>
2024-03-25 11:21:16 -0400

    bart new map

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect, useRef } from 'react';
 import './MapPage.css';
 import MapButton from '../components/MapButton';
 import MainHeader from '../components/MainHeader';
-import { graphql } from 'gatsby';
-import { find_path } from 'dijkstrajs';
-import { ReactPhotoSphereViewer } from 'react-photo-sphere-viewer'; // Import MapPlugin here
-import { Viewer } from '@photo-sphere-viewer/core';
-import { MapPlugin } from '@photo-sphere-viewer/map-plugin';
+import { GatsbyImage} from 'gatsby-plugin-image'
+import { Link, graphql } from 'gatsby';
 
+import { ReactPhotoSphereViewer } from 'react-photo-sphere-viewer';
+import { DijkstraCalculator } from 'dijkstra-calculator';
+import { GyroscopePlugin } from '@photo-sphere-viewer/gyroscope-plugin';
+import { MapPlugin } from '@photo-sphere-viewer/map-plugin';
 import '@photo-sphere-viewer/map-plugin/index.css';
 
-function RouteElement({ path, currentIndex }) {
+
+ 
+function RouteElement({path, currentIndex}) {
     let locations = []
 
     path.forEach((location, index) => {
@@ -22,13 +25,13 @@ function RouteElement({ path, currentIndex }) {
 
         // puts a div with location text and a div with just an arrow into locations array
         locations.push(<div key={location} className={`navitem ${thisItemBold ? "bold-location" : ""}`}>{location}</div>)
-        locations.push(<div key={location + "Arrow"} className='nav-item'>&nbsp; → &nbsp;</div>)
+        locations.push(<div key={location + "Arrow"} className='nav-item'>&nbsp; → &nbsp;</div>) 
     });
 
     if (locations.length > 0)
         locations.pop() // remove last arrow
 
-    // return constructed array of divs
+    // return constucted array of divs
     return (
         <div className='nav-route'>
             {locations}
@@ -36,107 +39,169 @@ function RouteElement({ path, currentIndex }) {
     )
 }
 
-function calculateWeight(location1, location2) {
+function calculateWeight(location1, location2){
     const deltaX = location1.x - location2.x;
     const deltaY = location1.y - location2.y;
 
     return Math.sqrt(deltaX ** 2 + deltaY ** 2);
+
 }
 
+
 // Take an array of edges, calculate weights, and create graph.
-function createGraph(edges) {
-    let graph = {};
+function createGraph(locations, edges) {
+    const graph = new DijkstraCalculator();
+
+    locations.map((location) => {
+        graph.addVertex(location.node.name);
+    })
+
     edges.map((edge) => {
         const weight = calculateWeight(edge.node.location1, edge.node.location2);
         // location1.name: {location2.name: weight}
-        graph[edge.node.location1.name] = { [edge.node.location2.name]: weight }
+        graph.addEdge(edge.node.location1.name, edge.node.location2.name, weight);
     });
     return graph;
 }
 
-const MapPage = ({ data, location }) => {
-    // Initialize photoSphereRef as a ref
-    const photoSphereRef = useRef(null);
+const MapPage = ({ data, location } ) => {
 
-    const maps = data.allSanityMap.nodes;
+    const photoSphereRef = React.createRef();
 
-    let start = location.state.startName;
-    let end = location.state.endName;
+    const maps = data.allSanityMap.nodes; // Use this to access an array of the maps.
 
-    let [currentIndex, setCurrentIndex] = useState(0);
+    let [currentIndex, setCurrentIndex] = useState(0); // The current index into the path array
     let [path, setPath] = useState([]);
+    let [reverse, setReverse] = useState(false);
 
+    let start = null;
+    let end = null;
+    
+    if(typeof location.state !== "undefined") {
+        start = location.state.startName;
+        end = location.state.endName;
+    } 
+        
+    // Create path
     useEffect(() => {
         if (start === undefined || end === undefined) {
             setPath([]);
             setCurrentIndex(0);
             return;
         }
-
+        
         const edges = data.allSanityEdge.edges;
-        const graph = createGraph(edges);
-        setPath(find_path(graph, start, end));
+        const locations = data.allSanityLocation.edges;
+        const graph = createGraph(locations, edges);
+        setPath(graph.calculateShortestPath(start, end));
         setCurrentIndex(0);
     }, [start, end]);
 
+    // Update image.
     useEffect(() => {
-        if (!photoSphereRef.current) return;
+      if (path.length <= 0) return;
+      const newCurrentLocation = data.allSanityLocation.edges.find(location => location.node.name === path[currentIndex]).node;
+      photoSphereRef.current.setPanorama(newCurrentLocation.image3D.asset.publicUrl, {transition: false, position: {yaw: reverse ? 3.2 : 0, pitch: 0}});
+      
+      // Calculate the angle towards the next location
+      let angle = 0;
+      if (currentIndex < path.length - 1) {
+        const nextLocation = data.allSanityLocation.edges.find(location => location.node.name === path[currentIndex+1]).node;
+        const deltaX = nextLocation.x - newCurrentLocation.x;
+        const deltaY = nextLocation.y - newCurrentLocation.y;
+        angle = Math.atan2(deltaY, deltaX);
 
-        const newCurrentLocation = data.allSanityLocation.edges.find(location => location.node.name === path[currentIndex]).node;
-        // Check if the setPanorama method is available before calling it
-        if (photoSphereRef.current.setPanorama) {
-            // Update panorama URL using setPanorama method
-            photoSphereRef.current.setPanorama(newCurrentLocation.image3D.asset.publicUrl);
+        // Adjust the angle to be within the range of 0 to 6.28 (2 * Math.PI)
+        angle = (angle >= 0 ? angle : (2 * Math.PI + angle)) + Math.PI/2;
+      }
 
-            // Update center coordinates dynamically
-            const center = { x: newCurrentLocation.x, y: newCurrentLocation.y };
-            photoSphereRef.current.getPlugin(MapPlugin).setCenter(center);
-        }
-    }, [path, currentIndex, data.allSanityLocation]);
+      // Update center coordinates dynamically
+      const center = { x: newCurrentLocation.x, y: newCurrentLocation.y };
+
+      //Update floor based on image
+      const currentFloor = newCurrentLocation.floor; // get current floor from current location
+      const currentFloorMap = maps.find(map => map.floor === currentFloor); // find the map object that has the current floor
+      photoSphereRef.current.getPlugin(MapPlugin).setImage(currentFloorMap.image.asset.publicUrl, center, angle);
+
+    }, [path, currentIndex, reverse]);
+
 
     function nextLocation() {
         if (currentIndex < path.length - 1) {
             setCurrentIndex(currentIndex + 1);
         }
     }
-
-    useEffect(() => {
-        // Initialize Viewer object within useEffect
-        photoSphereRef.current = new Viewer({
-            container: document.getElementById('map-container'),
-            panorama: '',
-            plugins: [
-                [MapPlugin, {
-                    imageUrl: maps[0].image.asset.publicUrl,
-                    center: { x: 1035, y: 727 }, // Default coordinates
-                    rotation: '-12deg',
-                }]
-            ]
-        });
-
-        // Cleanup function to destroy Viewer instance
-        return () => {
-            photoSphereRef.current.destroy();
-        };
-    }, []);
+    function prevLocation() {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    }
+    function returnToStart() {
+        setPath(path.reverse())
+        setCurrentIndex(0);
+        setReverse(true);
+    }
 
     return (
         <div className='MapPage'>
             <MainHeader />
             <div className="controlBox">
                 <MapButton link=".." displayText="New Entrance" />
-                <MapButton link={"../" + start} displayText="New Destination" />
-                <div className="destBox">
-                    <div className="buttonDiv">
-                        <button className="customButton" onClick={nextLocation}>
-                            Next Image
-                        </button>
-                    </div>
-                </div>
+                <Link to={`/ChooseEnd/`} state={{ startName: start}}>
+                    <MapButton link={""} displayText="New Destination" />
+                </Link>
+                {!reverse || (reverse && currentIndex < path.length - 1) ? (
+                    <>
+                        {currentIndex > 0 && (
+                            <div className="destBox">
+                                <div className="buttonDiv">
+                                    <button className="customButton" onClick={prevLocation}>
+                                        Previous Image
+                                    </button>
+                                </div>
+                            </div>
+                        )}
+                        {currentIndex < path.length - 1 && (
+                            <div className="destBox">
+                                <div className="buttonDiv">
+                                    <button className="customButton" onClick={nextLocation}>
+                                        Next Image
+                                    </button>
+                                </div>
+                            </div>
+                        )}
+                        {currentIndex === path.length - 1 && path[0] !== end && (
+                            <div className="destBox">
+                                <div className="buttonDiv">
+                                    <button className="customButton" onClick={returnToStart}>
+                                        Return to start
+                                    </button>
+                                </div>
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <h1 className="">You have returned to the start.</h1>
+                )}
+
             </div>
 
-            <div className="mapContainer" id="map-container">
-                <RouteElement path={path} currentIndex={currentIndex} className="navRoute" />
+
+            <div className="mapContainer">
+                <RouteElement path={path} currentIndex={currentIndex} className="navRoute"/>
+                <ReactPhotoSphereViewer 
+                    ref={photoSphereRef} 
+                    height={'70vh'} 
+                    width={"100%"}
+                    plugins={[
+                      [GyroscopePlugin],
+                      [MapPlugin, {
+                          imageUrl: '',
+                          center: { x: 1035, y: 727 }, // Default coordinates
+                          rotation: '0deg',
+                      }]
+                  ]}                   
+                />
             </div>
         </div>
     );
@@ -146,55 +211,55 @@ export const query = graphql`
 query MyQuery {
     allSanityMap {
         nodes {
-            floor
-            image {
-                asset {
-                    publicUrl
-                }
+          floor
+          image {
+            asset {
+              publicUrl
             }
+          }
         }
-    }
+      }
     allSanityEdge {
-        edges {
-            node {
-                location1 {
-                    name
-                    floor
-                    x
-                    y
-                    type
-                }
-                location2 {
-                    name
-                    type
-                    x
-                    y
-                    floor
-                }
-            }
+      edges {
+        node {
+          location1 {
+            name
+            floor
+            x
+            y
+            type
+          }
+          location2 {
+            name
+            type
+            x
+            y
+            floor
+          }
         }
+      }
     }
     allSanityLocation {
         edges {
-            node {
-                floor
-                name
-                type
-                x
-                y
-                image2D {
-                    asset {
-                        publicUrl
-                    }
-                }
-                image3D {
-                    asset {
-                        publicUrl
-                    }
-                }
+          node {
+            floor
+            name
+            type
+            x
+            y
+            image2D {
+              asset {
+                publicUrl
+              }
             }
+            image3D {
+              asset {
+                publicUrl
+              }
+            }
+          }
         }
-    }
-}`;
+      }
+  }`
 
 export default MapPage;
