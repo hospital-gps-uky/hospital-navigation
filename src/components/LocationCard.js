e21908c900ad8dacfbf0b04e9ec6b9d6a4b016a2
justin-t12 <justin.t12@outlook.com>
2024-11-06 16:27:41 -0500

    LocationCard: FIX fail to load placeholder image at build time
    Fixes bug where Gatsby would crash when trying to add placeholder image
    for location that did not have a 2D image.
    
    Fix included making separate components for standard thumbnail and
    placeholder thumbnail, instead of switching which image to load.
    
    Also adds new required package "gatsby-source-filesystem", which is a
    required plugin to ensure that the placeholder image file can be loaded
    at build time.
    
    Note that the error messages was quite vague and it not very specific
    about what exactly is causing the error, and also reports other info
    that is unrelated and somewhat misleading.
    
    Changes originally created by Colin Carver.

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import './LocationCard.css';
-import { GatsbyImage} from 'gatsby-plugin-image'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { StaticImage} from 'gatsby-plugin-image'
+// import { SourceFilesystem} from 'gatsby-source-filesystem'
 import { motion } from 'framer-motion';
 
-import placeHolder from '../images/1st Floor Main Entrance.png';
+// import placeHolder from '../images/1st Floor Main Entrance.png';
 
 const LocationCard = (location) => {
 
     return (
-    <motion.div initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5, duration: 0.6 }}>
-        <div className="card container">
-                <GatsbyImage 
-                    image={location.data.node.image2D ? location.data.node.image2D.asset.gatsbyImageData : placeHolder} 
-                    alt={location.data.node.name}
-                    style={{maxHeight: '250px'}}/>
+        <motion.div initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.6 }}>
+            {/* Check if 2D thumbnail image exists, if not, use a placeholder */}
+            <div className="card container">
+                {location.data.node.image2D ? (
+                    <GatsbyImage
+                        image={location.data.node.image2D.asset.gatsbyImageData}
+                        alt={location.data.node.name}
+                        style={{ maxHeight: '250px' }}
+                    />
 
+                ) : (
+                    /* Note that this needs to have gatsby-source-filesystem
+                     * installed for the placeholder image to be pulled */
+                    <StaticImage
+                        src="../images/1st Floor Main Entrance.png"
+                        alt="Placeholder"
+                        style={{ maxHeight: '250px' }}
+                    />
+                )}
                 <div className="column">
                     <p className="locationName">{location.data.node.name}</p>
                 </div>
-        
-        </div>
-    </motion.div>
+            </div>
+        </motion.div>
     )
 
 }
